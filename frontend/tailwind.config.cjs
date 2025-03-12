557eb800819cd1147e328047a9ab2e635c17344e
Lokesh <lvsharma2101@gmail.com>
2025-03-12 18:07:22 +0530

    production ready app

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// import { defineConfig } from 'tailwindcss';
+ module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
